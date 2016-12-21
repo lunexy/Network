e5fc8d60ee17011bcd796332de28379d754ce0d8
Lunexy Solutions <lunexy@users.noreply.github.com>
2016-12-21 04:41:13 +0200

    Update and rename vst-install-debian.sh to lnx-install.sh

--- lnx-install.sh
+++ lnx-install.sh
@@ -17,6 +17,12 @@ release=$(cat /etc/debian_version|grep -o [0-9]|head -n1)
 codename="$(cat /etc/os-release |grep VERSION= |cut -f 2 -d \(|cut -f 1 -d \))"
 vestacp="http://$CHOST/$VERSION/$release"
 
+
+
+
+
+if [ "$release" -eq 8 ]; then
+    software="nginx apache2 apache2-utils apache2.2-common
 apt-get install libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev
 cd /opt
 wget http://nginx.org/download/nginx-1.11.5.tar.gz
@@ -26,12 +32,7 @@ tar xf openssl-1.0.2j.tar.gz
 cd nginx-1.11.5
 ./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-openssl=/opt/openssl-1.0.2j
 make
-make install
-service nginx restart
-
-
-if [ "$release" -eq 8 ]; then
-    software="nginx apache2 apache2-utils apache2.2-common
+make install    
         apache2-suexec-custom libapache2-mod-ruid2
         libapache2-mod-fcgid libapache2-mod-php5 php5 php5-common php5-cgi
         php5-mysql php5-curl php5-fpm php5-pgsql awstats webalizer vsftpd
@@ -44,6 +45,16 @@ if [ "$release" -eq 8 ]; then
         bsdmainutils cron vesta vesta-nginx vesta-php expect libmail-dkim-perl"
 else
     software="nginx apache2 apache2-utils apache2.2-common
+apt-get install libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev
+cd /opt
+wget http://nginx.org/download/nginx-1.11.5.tar.gz
+wget https://www.openssl.org/source/openssl-1.0.2j.tar.gz
+tar xf nginx-1.11.5.tar.gz
+tar xf openssl-1.0.2j.tar.gz
+cd nginx-1.11.5
+./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-openssl=/opt/openssl-1.0.2j
+make
+make install    
         apache2-suexec-custom libapache2-mod-ruid2
         libapache2-mod-fcgid libapache2-mod-php5 php5 php5-common php5-cgi
         php5-mysql php5-curl php5-fpm php5-pgsql awstats webalizer vsftpd
